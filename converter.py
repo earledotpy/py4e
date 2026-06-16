6a830232d8e5a0f0c6cf424a1b6f6c950c1fd452
Jeremy Earle <jeremyaearle@gmail.com>
2026-06-16 10:56:46 -0600

    Change lesson structure to folder/file

--- converter.py
+++
@@ -1,4 +0,0 @@
-print ("Celcuis to Fahrenheit")
-cel = float(input("Celcius:"))
-fah = (cel* 1.8) + 32
-print("Fahrenheit:",fah)
--- count.py
+++
@@ -1,9 +0,0 @@
-def count(word, exact_letter):
-    total = 0
-    for letter in word:
-        if letter == exact_letter:
-            total = total + 1
-    return total
-
-print(count('banana', 'a'))
-
