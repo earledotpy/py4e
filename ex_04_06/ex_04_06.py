3bc0a7581f5acf4b4917c7edcd36ba57bc15c41f
Jeremy Earle <jeremyaearle@gmail.com>
2026-07-14 15:42:42 -0600

    Refactor computepay to accept parameters and return pay

--- ex_04_06/ex_04_06.py
+++ ex_04_06/ex_04_06.py
@@ -1,8 +1,8 @@
-def computepay():
-    if hr > 40:
-        pay = rt * 40 + (rt *1.5) * (hr - 40)
-    else : pay = hr * rt
-    print(pay)
+def computepay(hr, rt):
+    if hr <= 40:
+        return hr * rt
+    return rt * 40 + (rt *1.5) * (hr - 40)
 hr = float(input("Enter Hours: "))
 rt = float(input("Enter Rate: "))
-computepay()
+py = computepay(hr, rt)
+print('Pay', py)
